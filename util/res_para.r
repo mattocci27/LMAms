aeba58133218e2c1bec16df38b59b0626883a0aa
Masatoshi Katabuchi <mattocci27@gmail.com>
2019-10-22 11:45:52 +0800

    clean

--- util/res_para.r
+++ util/res_para.r
@@ -1,5 +1,6 @@
 library(rstan)
-load("./data/GL_m0_obs.rda")
+library(tidyverse)
+load("./data/GL_LMAms_obs.rda")
 
 s_GL <- data.frame(summary(res)$summary) %>%
   mutate(para = rownames(.)) %>%
@@ -10,22 +11,11 @@ s_GL <- data.frame(summary(res)$summary) %>%
          "sig",
          sig))
 
-write.csv(s_GL, "./data/GLpara.csv")
+write_csv(s_GL, "./data/GLpara.csv")
 
-load("./data/GL_m0_N_obs.rda")
 
-s_GL <- data.frame(summary(res)$summary) %>%
-  mutate(para = rownames(.)) %>%
-  mutate(sig = ifelse(`X2.5.` < 0 & `X97.5.` < 0 ,
-         "sig",
-         "nonsig")) %>%
-  mutate(sig = ifelse(`X2.5.` > 0 & `X97.5.` > 0 ,
-         "sig",
-         sig))
+load("./data/PA_LMAms_L_more_obs.rda")
 
-write.csv(s_GL, "./data/GLparaN.csv")
-
-load("./data/PA_m1q_more_obs.rda")
 s_PA <- data.frame(summary(res)$summary) %>%
   mutate(para = rownames(.)) %>%
   mutate(sig = ifelse(`X2.5.` < 0 & `X97.5.` < 0 ,
@@ -34,27 +24,6 @@ s_PA <- data.frame(summary(res)$summary) %>%
   mutate(sig = ifelse(`X2.5.` > 0 & `X97.5.` > 0 ,
          "sig",
          sig))
-write.csv(s_PA, "./data/PApara.csv")
-
-load("./data/PA_m1q_more_N_obs.rda")
-s_PA <- data.frame(summary(res)$summary) %>%
-  mutate(para = rownames(.)) %>%
-  mutate(sig = ifelse(`X2.5.` < 0 & `X97.5.` < 0 ,
-         "sig",
-         "nonsig")) %>%
-  mutate(sig = ifelse(`X2.5.` > 0 & `X97.5.` > 0 ,
-         "sig",
-         sig))
-write.csv(s_PA, "./data/PAparaN.csv")
+write_csv(s_PA, "./data/PApara.csv")
 
 
-load("./data/PA_m1q_more_NS_more_obs.rda")
-s_PA <- data.frame(summary(res)$summary) %>%
-  mutate(para = rownames(.)) %>%
-  mutate(sig = ifelse(`X2.5.` < 0 & `X97.5.` < 0 ,
-         "sig",
-         "nonsig")) %>%
-  mutate(sig = ifelse(`X2.5.` > 0 & `X97.5.` > 0 ,
-         "sig",
-         sig))
-write.csv(s_PA, "./data/PAparaNS.csv")
