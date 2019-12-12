ce5367639079ceca4dad9c0d2ead22d559370454
Masatoshi Katabuchi <mattocci27@gmail.com>
2019-12-12 11:55:13 +0800

    nohup

--- sh/model_obs_cloud.sh
+++ sh/model_obs_cloud.sh
@@ -3,11 +3,11 @@ DATA=GL
 OBS=obs
 for MODEL in GL_LMAms
 do
-    R --vanilla --slave --args ${MODEL} ${DATA} 4 3 1 ${OBS} < ./model/model_more.r > ./log/${DATA}_${MODEL}_${OBS}.log
+    nohup R --vanilla --slave --args ${MODEL} ${DATA} 4 3 1 ${OBS} < ./model/model_more.r > ./log/${DATA}_${MODEL}_${OBS}.log &
 done
 
 DATA=PA
 for MODEL in PA_LMAms_L0 PA_LMAms0
 do
-    R --vanilla --slave --args ${MODEL} ${DATA} 4 3 1 ${OBS} < ./model/model_more.r > ./log/${DATA}_${MODEL}_${OBS}.log
+    nohup R --vanilla --slave --args ${MODEL} ${DATA} 4 3 1 ${OBS} < ./model/model_more.r > ./log/${DATA}_${MODEL}_${OBS}.log &
 done
--- sh/model_rand_cloud.sh
+++ sh/model_rand_cloud.sh
@@ -3,9 +3,9 @@ OBS=rand
 DATA=GL
 MODEL=GL_LMAms
 
-R --vanilla --slave --args ${MODEL} ${DATA} 4 3 1 ${OBS} < ./model/model.r > ./log/${MODEL}_${OBS}.log
+nohup R --vanilla --slave --args ${MODEL} ${DATA} 4 3 1 ${OBS} < ./model/model.r > ./log/${MODEL}_${OBS}.log &
 
 DATA=PA
 MODEL=PA_LMAms_L0
 
-R --vanilla --slave --args ${MODEL} ${DATA} 4 3 1 ${OBS} < ./model/model_more.r > ./log/${MODEL}_${OBS}.log
+nohup R --vanilla --slave --args ${MODEL} ${DATA} 4 3 1 ${OBS} < ./model/model_more.r > ./log/${MODEL}_${OBS}.log &
