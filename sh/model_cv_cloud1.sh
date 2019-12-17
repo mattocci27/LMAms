cf835bd3054fbea3674dd977082dd145696b89fb
Masatoshi Katabuchi <mattocci27@gmail.com>
2019-12-17 20:44:25 +0800

    16cpu

---
+++ sh/model_cv_cloud1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+DATA=GL
+OBS=obs
+for MODEL in GL_LMAms_CV GL_LMA_CV
+do
+  #
+  echo "${MODEL}, ${DATA}, ${OBS}"
+  export MODEL DATA OBS
+  nohup R --vanilla --slave --args ${MODEL} ${DATA} 4000 3000 1 ${OBS} < ./model/k_fold.r > ./log/${MODEL}_${OBS}.log &
+
+  sleep 1 # pause to be kind to the scheduler
+done
+
+DATA=PA
+for MODEL in PA_LMA_CV PA_LMA_L_CV 
+do
+  #
+  echo "${MODEL}, ${DATA}, ${OBS}"
+  export MODEL DATA OBS
+  nohup R --vanilla --slave --args ${MODEL} ${DATA} 4000 3000 1 ${OBS} < ./model/k_fold.r > ./log/${MODEL}_${OBS}.log &
+
+  sleep 1 # pause to be kind to the scheduler
+done
+wait
+
+
+python line.py "host - ${HOSTNAME}: MCMC for CV done!"
+
+sh ./sh/mv_dat.sh local_to_dropbox
+
+python line.py "${HOSTNAME}: moved local files to Dropbox!"
