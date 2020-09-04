132f0b4d55719c227d671f20afe09428883a471d
Masatoshi Katabuchi <mattocci27@gmail.com>
2020-09-04 17:57:17 +0800

    use loo

---
+++ util/get_loo.r
@@ -0,0 +1,49 @@
+library(tidyverse)
+library(rstan)
+library(stringr)
+library(loo)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+#load("./rda/GL_LMAms_more_obs.rda")
+#GL_LMAms <- res
+
+#load("./data/GL_LMA_more_obs.rda")
+#GL_LMA <- res
+obs_files <- list.files("rda") %>%
+  str_subset("obs.rda") 
+
+obs_files <- obs_files[!str_detect(obs_files, "_more") | 
+                 !str_detect(obs_files, "PA")] 
+
+tmp <- obs_files %>%
+  str_split_fixed("_obs", 2)
+
+models <- tmp[,1]
+n_samp <- NULL
+for (i in 1:length(models)) {
+  load(str_c("rda/", obs_files[i]))
+  assign(models[i], res)
+  n_samp[i] <- nrow(dat)
+}
+
+
+tmp2 <- models %>% str_split_fixed("_more", 2)
+models_clean <- tmp2[, 1]
+
+dat <- tibble(LMA = models_clean) %>%
+# head(2) %>%
+ mutate(N = n_samp) %>%
+ mutate(elpd_list = map(LMA, ~ loo(get(.)))) %>%
+ mutate(epld = map_dbl(elpd_list, ~ .$estimates[1, 1]))
+
+GL <- dat %>%
+  dplyr::select(LMA, epld, N) %>%
+  filter(str_detect(LMA, "GL")) %>%
+  arrange(desc(epld))
+
+PA <- dat %>%
+  dplyr::select(LMA, epld, N) %>%
+  filter(str_detect(LMA, "PA")) %>%
+  filter(!str_detect(models, "more")) %>% 
+  arrange(desc(epld))
