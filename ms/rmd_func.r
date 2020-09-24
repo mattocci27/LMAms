ddc3eb776b1f30f324e2ea1106366a01ca0dac83
mattocci <mattocci27@gmail.com>
2020-09-24 15:24:07 +0800

    fixed cross-ref #20

--- ms/rmd_func.r
+++ ms/rmd_func.r
@@ -8,27 +8,27 @@ page_break <- function(section = NULL){
 # from http://stackoverflow.com/a/18672268/1036500
 # library(knitr)
 
-to = local({
-  i <- 0
-  function(x) {
-    i <<- i + 1
-    paste("<table><caption>",
-    '**Table ****', i, '**: ', x,
-    "</caption><colgroup><col width='100'></col></colgroup><thead><tr class='header'></tr></thead><tbody></tbody></table><p>", sep = '')
-  }
-})
-knit_hooks$set(tab.cap = function(before, options, envir) {
-  if(before) # caption above tables
-    to(options$tab.cap)
-})
-default_output_hook = knit_hooks$get("output")
-knit_hooks$set(output = function(x, options) {
-  if (is.null(options$tab.cap) == F)
-    x
-  else
-    default_output_hook(x, options)
-})
-
+#to = local({
+#  i <- 0
+#  function(x) {
+#    i <<- i + 1
+#    paste("<table><caption>",
+#    '**Table ****', i, '**: ', x,
+#    "</caption><colgroup><col width='100'></col></colgroup><thead><tr class='header'></tr></thead><tbody></tbody></table><p>", sep = '')
+#  }
+#})
+#knit_hooks$set(tab.cap = function(before, options, envir) {
+#  if(before) # caption above tables
+#    to(options$tab.cap)
+#})
+#default_output_hook = knit_hooks$get("output")
+#knit_hooks$set(output = function(x, options) {
+#  if (is.null(options$tab.cap) == F)
+#    x
+#  else
+#    default_output_hook(x, options)
+#})
+#
 # This is a knitr hook to enable figure and table captions
 # number figures
 # from http://stackoverflow.com/a/18672268/1036500
