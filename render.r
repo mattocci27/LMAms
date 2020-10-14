fd673d17b6a568a1e5e8619d56978992bc5b06ff
Masatoshi Katabuchi <mattocci27@gmail.com>
2020-10-14 17:46:56 +0800

    fixed all the fig for main text

--- render.r
+++ render.r
@@ -5,6 +5,8 @@ system.time(render("./ms/LMAps_main_re.rmd", "bookdown::word_document2"))
 system.time(render("./ms/LMAps_main_re.rmd", "html_document"))
 system.time(render("./ms/SI.rmd", "html_document"))
 
-bookdown::render_book("./ms/SI/index.rmd", output_format = "bookdown::pdf_book")
-bookdown::render_book("./ms/SI/LMAps_SI.rmd", output_format = "bookdown::word_document2")
+bookdown::render_book(input = "./ms/SI/index.rmd", output_format = "bookdown::pdf_document2")
+bookdown::render_book("./ms/SI/index.rmd", output_format = "bookdown::word_document2")
 
+bookdown::render_book(input = "index.rmd", 
+                      output_format = "bookdown::word_document2")
