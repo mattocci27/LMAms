ecbc710255f99db37374ddae109405d0e0cf6580
Masatoshi Katabuchi <mattocci27@gmail.com>
2020-10-15 10:31:40 +0800

    LMAps to LMAms

--- render.r
+++ render.r
@@ -1,8 +1,8 @@
 library(rmarkdown)
 #system.time(render("./ms/LMAps_main.rmd", "bookdown::pdf_book"))
 #system.time(render("./ms/LMAps_main_re.rmd", "bookdown::pdf_book"))
-system.time(render("./ms/LMAps_main_re.rmd", "bookdown::word_document2"))
-system.time(render("./ms/LMAps_main_re.rmd", "html_document"))
+system.time(render("./ms/LMAms_main_re.rmd", "bookdown::word_document2"))
+system.time(render("./ms/LMAms_main_re.rmd", "html_document"))
 system.time(render("./ms/SI.rmd", "html_document"))
 
 bookdown::render_book(input = "./ms/SI/index.rmd", output_format = "bookdown::pdf_document2")
--- render.sh
+++ render.sh
@@ -2,11 +2,11 @@
 
 cp ~/MyLibrary.bib ./ms/MyLibrary.bib
 
-R -e 'system.time(render("./ms/LMAps_main_re.rmd", "bookdown::word_document2"))' && echo "word file completed!"
+R -e 'system.time(render("./ms/LMAms_main_re.rmd", "bookdown::word_document2"))' && echo "word file completed!"
 
-R -e 'system.time(render("./ms/LMAps_main_re.rmd", "bookdown::pdf_document2"))' && echo "PDF file completed!"
+R -e 'system.time(render("./ms/LMAms_main_re.rmd", "bookdown::pdf_document2"))' && echo "PDF file completed!"
 
-nohup R -e 'system.time(render("./ms/LMAps_main_re.rmd", "bookdown::html_document2"))' && echo "html file completed!" &
+nohup R -e 'system.time(render("./ms/LMAms_main_re.rmd", "bookdown::html_document2"))' && echo "html file completed!" &
 #nohup R -e 'system.time(render("./ms/LMAps_main_re.rmd", "html_document"))' && echo "html file completed!" &
 
 cd ms
