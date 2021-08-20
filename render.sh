cb401c09b0b76ea23e7cca0f9d307874b7b19e9b
Masatoshi Katabuchi <mattocci27@gmail.com>
2021-08-20 18:14:51 +0800

    clean

--- render.sh
+++ render.sh
@@ -2,16 +2,15 @@
 
 cp ~/LMA.bib ./ms/LMA.bib
 
-R -e 'system.time(render("./ms/LMAms_main_re.rmd", "bookdown::word_document2"))' && echo "word file completed!"
+#R -e 'system.time(rmarkdown::render("./ms/LMAms_main.rmd", "bookdown::word_document2"))' && echo "word file completed!"
 
-R -e 'system.time(render("./ms/LMAms_main_re.rmd", "bookdown::pdf_document2"))' && echo "PDF file completed!"
+R -e 'system.time(rmarkdown::render("./ms/LMAms_main.rmd", "bookdown::pdf_document2"))' && echo "PDF file completed!"
 
-nohup R -e 'system.time(render("./ms/LMAms_main_re.rmd", "bookdown::html_document2"))' && echo "html file completed!" &
-#nohup R -e 'system.time(render("./ms/LMAps_main_re.rmd", "html_document"))' && echo "html file completed!" &
+#nohup R -e 'system.time(rmarkdown::render("./ms/LMAms_main.rmd", "bookdown::html_document2"))' && echo "html file completed!" &
 
 cd ms
 
-#latexdiff LMAps_main.tex LMAps_main_re.tex > diff.tex
+#latexdiff LMAms_main_old.tex LMAms_main.tex > diff.tex
 
 #pdflatex -halt-on-error diff.tex
 #pdflatex diff.tex
