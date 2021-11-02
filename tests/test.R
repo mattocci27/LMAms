a4796adb1f25db7addb551962506aeaf6680dcc3
Masatoshi Katabuchi <mattocci27@gmail.com>
2021-11-02 10:45:53 +0800

    updated rand

--- tests/test.R
+++ tests/test.R
@@ -1,31 +1,35 @@
-library(tidyverse)
-library(rstan)
-library(stringr)
-library(loo)
-rstan_options(auto_write = TRUE)
-# options(mc.cores = parallel::detectCores())
-options(mc.cores = 4)
+library(cmdstanr)
+set_cmdstan_path("/opt/cmdstan/cmdstan-2.27.0")
 
-set.seed(5)
+library(tidyverse)
+#library(rstan)
+library(stringr)
+#library(loo)
+#rstan_options(auto_write = TRUE)
+# options(mc.cores = parallel::detectCores())
+#options(mc.cores = 4)
+
+set.seed(123)
 
 n_chains <- 4
-n_rand <- 10
-argv <- commandArgs(trailingOnly = TRUE)
-n_model <- argv[1]
-n_model2 <- paste0("./model/", n_model, ".stan")
-data_name <- argv[2]
-n_iter <- as.numeric(argv[3])
-n_warm <- as.numeric(argv[4])
-n_thin <- as.numeric(argv[5])
-obs <- argv[6]
-
-#n_model <- noquote("PA_LMAms_L0")
+n_rand <- 2
+#argv <- commandArgs(trailingOnly = TRUE)
+#n_model <- argv[1]
 #n_model2 <- paste0("./model/", n_model, ".stan")
-#data_name <- "PA"
-#n_iter <- 4
-#n_warm <- 1
-#n_thin <- 1
-#obs <- "rand"
+#data_name <- argv[2]
+#n_iter <- as.numeric(argv[3])
+#n_warm <- as.numeric(argv[4])
+#n_thin <- as.numeric(argv[5])
+#obs <- argv[6]
+
+n_model <- noquote("GL_Aps_LLs")
+n_model2 <- paste0("./model/", n_model, ".stan")
+data_name <- "GL"
+n_iter <- 4
+n_warm <- 3
+n_thin <- 1
+obs <- "rand"
+obs <- "obs"
 
 print(paste("Model:", n_model))
 print(paste("Model dir:", n_model2))
@@ -42,6 +46,9 @@ if (data_name == "GL") {
     as.data.frame
 
   list_dat <- list(N = nrow(dat),
+                   obs = cbind(log(dat[ , "Aarea"] + dat[ , "Rarea"]),
+                               log(dat[ , "LL"]),
+                               log(dat[ , "Rarea"])),
                    LMA = dat[ , "LMA"],
                    A = dat[ , "Aarea"],
                    LL = dat[ , "LL"],
@@ -49,13 +56,17 @@ if (data_name == "GL") {
                    q_lim = 1,
                    leaf = 1,
                    dry = 1,
-                   DE = as.numeric(as.factor(dat$DE)))
+                   gr = as.numeric(as.factor(dat$DE)))
+
+  list_dat$J <- list_dat$gr %>% unique %>% length
 
   list_dat$obs2 <- list_dat$obs
+  list_dat$E <- ifelse(dat$DE == "E", 1, 0)
+  list_dat$U <- ifelse(dat$DE == "U", 1, 0)
 
 } else if (data_name == "PA") {
 
-  dat <- read_csv("./data/PA_data_more.csv") %>%
+  dat <- read_csv("./data/PA_data.csv") %>%
     filter(!is.na(LMA)) %>%
     filter(!is.na(Aarea)) %>%
     filter(!is.na(Rarea)) %>%
@@ -72,14 +83,16 @@ if (data_name == "GL") {
     as.numeric()
 
   list_dat <- list(N = nrow(dat),
+                   obs = cbind(log(dat[ , "Aarea"] + dat[ , "Rarea"]),
+                               log(dat[ , "LL"]),
+                               log(dat[ , "Rarea"])),
                    LMA = dat[ , "LMA"],
-                   #LT = dat[ , "LT"],
+                   LT = dat[ , "LT"],
                    A = dat[ , "Aarea"],
                    LL = dat[ , "LL"],
                    R = dat[ , "Rarea"],
                    DE = 1,
                    gr = dat$gr,
-                   jj = dat$gr,
                    J = dat$gr %>% unique %>% length,
                    q_lim = q_lim,
                    #leaf = as.numeric(as.factor(dat$strata)),
@@ -92,18 +105,18 @@ rand_dat <- tibble(null_model = 1:n_rand)
 rand_fun <- function(n){
   temp <- data.frame(dat[,1:3],
            LMA = sample(dat$LMA),
-           #LMA = dat$LMA,
            LL = sample(dat$LL),
            Aarea = sample(dat$Aarea),
-           Rarea = sample(dat$Rarea)
+           Rarea = sample(dat$Rarea),
+           LT = sample(dat$Rarea)
            )
   while (min(temp$Aarea - temp$Rarea) < 0){
   temp <- data.frame(dat[,1:3],
            LMA = sample(dat$LMA),
-           #LMA = dat$LMA,
            LL = sample(dat$LL),
            Aarea = sample(dat$Aarea),
-           Rarea = sample(dat$Rarea)
+           Rarea = sample(dat$Rarea),
+           LT = sample(dat$Rarea)
            )
   }
 
@@ -113,10 +126,13 @@ rand_fun <- function(n){
             A = temp$Aarea,
             LL = temp$LL,
             R = temp$Rarea,
+            LT = temp$LT,
             q_lim = list_dat$q_lim,
             leaf = list_dat$leaf,
             dry = list_dat$dry,
-            DE = list_dat$DE,
+            #DE = list_dat$DE,
+            gr = list_dat$gr,
+            J = list_dat$J,
             LMA = temp$LMA)
 }
 
@@ -142,7 +158,23 @@ if (obs == "obs") {
              seed = 123,
              control = list(adapt_delta = 0.99, max_treedepth = 15)))
 
-  save_name <- paste("./rda/", n_model, "_more_obs.rda", sep = "")
+   tic()
+    res <- model_cl$sample(
+            data = list_dat,
+            iter_sampling = 10,
+            iter_warmup = 30,
+            thin = n_thin,
+            chains = n_chains,
+	    parallel_chains  = n_chains,
+            seed = 123,
+	    refresh = 200,
+            adapt_delta = 0.99,
+	    max_treedepth = 15)
+  toc()
+
+  /tmp/RtmpKJ2Mdz/model-1c631ea899d.stan
+
+  save_name <- paste("./rda/", n_model, "_obs.rda", sep = "")
 }
 
 if (obs == "rand") {
@@ -154,15 +186,21 @@ if (obs == "rand") {
   #            warmup = 0,
   #            thin = 1,
   #            chains = 1))
+  model_cl <- cmdstan_model("model/GL_Aps_LLs.stan",
+                        cpp_options = list(stan_opencl = TRUE))
 
   mod_fun <- function(data){
-    res <- stan(file = n_model2,
+    res <- model_cl$sample(
             data = data,
-            iter = n_iter,
-            warmup = n_warm,
+            iter_sampling = 1000,
+            iter_warmup = 3000,
             thin = n_thin,
             chains = n_chains,
-            control = list(adapt_delta = 0.99, max_treedepth = 15))
+	    parallel_chains  = n_chains,
+            seed = 123,
+	    refresh = 200,
+            adapt_delta = 0.99,
+	    max_treedepth = 15)
     res
   }
 
@@ -170,12 +208,16 @@ if (obs == "rand") {
      data.frame(summary(model)$summary)
   }
 
-  set.seed(123)
+  library(tictoc)
+  tic()
   rand_res <- rand_dat %>%
-    mutate(model = map(data, mod_fun)) %>%
-    mutate(summary = map(model, summary_fun))
+    mutate(model = map(data, mod_fun))
+   toc()
 
-  save_name <- paste("./rda/", n_model, "_more_rand.rda", sep = "")
+#     %>%
+#    mutate(summary = map(model, summary_fun))
+
+  save_name <- paste("./rda/", n_model, "_rand.rda", sep = "")
 }
 
 save.image(save_name)
