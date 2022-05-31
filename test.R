916d1a2e37c7ad3f2a231a2968a5b9caa50d199c
Masatoshi Katabuchi <mattocci27@gmail.com>
2022-05-31 19:49:34 +0900

    run rand

--- test.R
+++ test.R
@@ -1,6 +1,72 @@
 # sudo singularity build singularity.sif singularity.def
 
-targets::tar_read(gl_stan_dat)
+library(targets)
+library(tarchetypes)
+library(tidyverse)
+library(stantargets)
+library(cmdstanr)
+
+targets::tar_load(gl_csv)
+targets::tar_load(gl_stan_dat)
+targets::tar_load(pa_rand_list)
+targets::tar_load(pa_rand_fit)
+
+pa_rand_fit
+
+# pa_rand_list
+# pa_rand_list
+
+list_dat <- gl_stan_dat
+dat <- read_csv(gl_csv) %>%
+    mutate(DE = ifelse(is.na(DE), "U", DE)) %>%
+    as.data.frame()
+
+hoge <- list()
+for (i in 1:3) hoge[[i]] <- rand_fun(dat)
+
+moge <- tibble(hoge, rep = 1:3)
+tar_group_by(hello, moge, rep)
+
+targets::tar_load(pa_rand_fit)
+pa_rand_fit
+
+targets::tar_load(gl_rand_fit)
+gl_rand_fit
+
+rand_dat <- rand_dat %>%
+  mutate(data = map(1:n_rand, rand_fun, ))
+
+rand_fun <- function(n, dat, list_dat){
+  temp <- data.frame(dat[,1:3],
+           LMA = sample(dat$LMA),
+           #LMA = dat$LMA,
+           LL = sample(dat$LL),
+           Aarea = sample(dat$Aarea),
+           Rarea = sample(dat$Rarea)
+           )
+  while (min(temp$Aarea - temp$Rarea) < 0){
+  temp <- data.frame(dat[,1:3],
+           LMA = sample(dat$LMA),
+           #LMA = dat$LMA,
+           LL = sample(dat$LL),
+           Aarea = sample(dat$Aarea),
+           Rarea = sample(dat$Rarea)
+           )
+  }
+
+  temp$A_R <- temp$A - temp$R
+
+  list(N = list_dat$N,
+            A = temp$Aarea,
+            LL = temp$LL,
+            R = temp$Rarea,
+            q_lim = list_dat$q_lim,
+            leaf = list_dat$leaf,
+            dry = list_dat$dry,
+            DE = list_dat$DE,
+            LMA = temp$LMA)
+}
+
 
 targets::tar_read(fit_1_summary_GL_Aps_LLs)
 targets::tar_read(fit_1_diagnostics_GL_Aps_LLs)
