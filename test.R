c2bfce6b8b3aa9143b6bbf9039ed16458aacc9ab
Masatoshi Katabuchi <mattocci27@gmail.com>
2022-06-02 12:41:12 +0900

    update rand data

--- test.R
+++ test.R
@@ -9,9 +9,73 @@ library(cmdstanr)
 targets::tar_load(gl_csv)
 targets::tar_load(gl_stan_dat)
 targets::tar_load(pa_rand_list)
-targets::tar_load(pa_rand_fit)
 
-pa_rand_fit
+targets::tar_load(gl_rand_list)
+targets::tar_load(gl_rand_fit)
+dat <- gl_rand_list$data[[12]]
+gl_rand_fit[[12]]$summary |> head(20)
+
+lm(log(A) ~ log(LMA), dat) |> summary()
+lm(log(LL) ~ log(LMA), dat) |> summary()
+
+
+targets::tar_load(gl_csv)
+data <- read_csv(gl_csv)
+  tmp <- data.frame(data[,1:3],
+           LMA = sample(data$LMA),
+           #LMA = data$LMA,
+           LL = sample(data$LL),
+           Aarea = sample(data$Aarea),
+           Rarea = sample(data$Rarea)
+           )
+
+a_pval <- cor.test(log(tmp$LMA), log(tmp$Aarea))$p.val
+l_pval <- cor.test(log(tmp$LMA), log(tmp$LL))$p.val
+r_pval <- cor.test(log(tmp$LMA), log(tmp$Rarea))$p.val
+
+a_pval
+l_pval
+r_pval
+
+gl_rand_fit[[3]]$summary
+gl_rand_fit[[8]]$summary
+
+gl_rand_fit[[9]]$summary |>
+  filter(rhat > 1.05)
+
+hoge <- gl_rand_fit[[12]]$draws
+as <- hoge[, ,"as"]
+as.numeric(ap) |> quantile(0.025)
+
+apply(as, 2, \(x)quantile(x, 0.975))
+
+pmat <- hoge[,,paste0("p[",1:198,"]")]
+
+targets::tar_load(pa_rand_fit)
+targets::tar_load(pa_rand_list)
+
+hoge <- pa_rand_fit[[3]]$draws
+hoge[1,1,paste0("p[",1:106,"]")]
+
+dim(hoge)
+names(hoge[1,1,])
+  as_tibble() |>
+|> janitor::clean_names()
+
+str(pa_rand_fit[[3]])
+  draws <- draws |>
+    janitor::clean_names()
+  p_dat <- draws |>
+    dplyr::select(contains("p_"))
+  p_vec <- apply(p_dat, 2, mean)
+
+
+
+hoge <- pa_rand_list$data[[3]]
+
+plot(hoge$A, hoge$LMA, log = "xy")
+plot(hoge$LL, hoge$LMA, log = "xy")
+plot(hoge$R, hoge$LMA, log = "xy")
 
 # pa_rand_list
 # pa_rand_list
