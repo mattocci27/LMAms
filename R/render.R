c2b917d8e882f97591bd96a17b6dc96ef70afba2
Masatoshi Katabuchi <mattocci27@gmail.com>
2022-10-19 16:20:03 +0800

    clean

--- R/render.R
+++ R/render.R
@@ -5,10 +5,10 @@
 # - First define the "full" version, which would include a caption
 # (except I never use the caption in the function, and instead replace with 'blank')
 
-fig_full <- captioner::captioner(prefix = "Figure")
+fig_full <- captioner::captioner(prefix = "Fig.")
 table_full <- captioner::captioner(prefix = "Table")
-eq_full <- captioner::captioner(prefix = "Equation")
-s_fig_full <- captioner::captioner(prefix = "Figure S", auto_space = FALSE)
+eq_full <- captioner::captioner(prefix = "Eq.")
+s_fig_full <- captioner::captioner(prefix = "Fig. S", auto_space = FALSE)
 s_table_full <- captioner::captioner(prefix = "Table S", auto_space = FALSE)
 
 # - Make a short function that prints only the object type and number, e.g., "Figure 1"
@@ -20,8 +20,8 @@ s_table <- pryr::partial(s_table_full, display = "cite", caption = "blank")
 
 # - Make a short function that prints only the number (e.g., "1")
 # |> doen't work
-fig_num <- function (name) {fig(name) %>% str_remove("Figure ")}
+fig_num <- function (name) {fig(name) %>% str_remove("Fig. ")}
 table_num <- function (name) {table(name) %>% str_remove("Table ")}
-eq_num <- function (name) {eq(name) %>% str_remove("Equation ")}
-s_fig_num <- function (name) {s_fig(name) %>% str_remove("Figure ")}
+eq_num <- function (name) {eq(name) %>% str_remove("Eq. ")}
+s_fig_num <- function (name) {s_fig(name) %>% str_remove("Fig. ")}
 s_table_num <- function (name) {s_table(name) %>% str_remove("Table ")}
